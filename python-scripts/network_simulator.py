0c546d09506f030bee151336f6641cd6d2ba45fd
RonaldoBaker <r.remario.baker@gmail.com>
2024-08-18 09:21:06 +0100

    New simulator class

---
+++ python-scripts/network_simulator.py
@@ -0,0 +1,26 @@
+import networkx as nx
+import torch
+import torch.nn as nn
+import random
+import time
+import bisect
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from neural_networks import Generator
+
+
+# Constants
+GENERATOR_PATH = "../trained-generator/generator.pt"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+RANDOM_SEED = 77
+MIN_FLOWS_SENT = 1e2
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
