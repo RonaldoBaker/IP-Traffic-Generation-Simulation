e2485375fc664aa108429e0efa523b967f663728
RonaldoBaker <143547412+RonaldoBaker@users.noreply.github.com>
2024-09-21 14:27:39 +0100

    Add run simulation function

--- python-scripts/simulation/network_simulation.py
+++ python-scripts/simulation/network_simulation.py
@@ -1,17 +1,23 @@
 import torch
+import numpy as np
+from simulator import Simulator
 
 # Constants
 ARCHITECTURE = ""
 GENERATOR_PATH = "../../trained-generator/generator.pt"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+GRAPH = "../../uk-net/UKnet.txt"
+ADDRESSES = [i for i in range(1, 22)]
+WAVELENGTHS = 40
 RANDOM_SEED = 77
 MIN_FLOWS_SENT = 1e2
+EXPON_SCALE = 0.1
+EXPON_DIST = np.random.exponential(scale = EXPON_SCALE, size = 20000000)
 
 
 def main():
-
-    pass
-
+    simulator = Simulator(GENERATOR_PATH, RANDOM_SEED, DEVICE, ADDRESSES, GRAPH, WAVELENGTHS, MIN_FLOWS_SENT, EXPON_DIST)
+    simulator.run_simulation()
 
 if __name__ == "__main__":
     main()
--- python-scripts/simulation/simulator.py
+++ python-scripts/simulation/simulator.py
@@ -1,26 +1,35 @@
 import torch
+import time
 import bisect
 import operator
 import networkx as nx
+import numpy as np
+from tqdm import tqdm
 from networks import ConvergedNetwork, NonConvergedNetwork
 from flow_factory import FlowFactory
 from ..training.neural_networks import Generator
 from event import Event
 class Simulator:
-    def __init__(self, generator_path, seed, device, addresses, graph, num_wavelengths):
+    def __init__(self, generator_path, seed, device, addresses, graph, num_wavelengths, min_flows, expon_dist):
         self.generator_path = generator_path
         self.seed = seed
         self.device = device
         self.addresses = addresses
         self.graph = graph
         self.num_wavelengths = num_wavelengths
+        self.min_flows = min_flows
+        self.expon_dist = expon_dist
         self.sim_clock = 0
         self.sent = 0
         self.blocked = 0
         self.assortment_key = operator.attrgetter("event_time")
         self.events = list()
         self.durations = list()
-        self.__set_architecture()
+        self.iat_times = list()
+        self.start_time = 0
+        self.end_time = 0
+        self.run_time = 0
+        self.__initialise()
 
     def __set_architecture(self):
         print("What kind of architecture would you like to simulate today?")
@@ -48,11 +57,6 @@ class Simulator:
         new_event = Event(event_time, event_type, associated_flow)
         bisect.insort(self.events, new_event, key=self.assortment_key)
 
-    def __initialise_events_list(self):
-        # new_flow = self.flow_generator.generate_flow()
-        # self.durations.append(new_flow.dur)  # Cannot remember what I actually use this for?
-        self.__schedule_event(0, "ARRIVAL", self.flow_factory.generate_flow())
-
     def __initialise(self):
         self.__set_architecture()
         self.__set_network()
@@ -65,6 +69,51 @@ class Simulator:
             self.addresses)
         self.__schedule_event(0, "ARRIVAL", self.flow_factory.generate_flow())
 
+    def __timing_routine(self):
+        next_event = self.events.pop(0)
+        advanced_time = next_event.event_time
+        self.sim_clock = advanced_time
+        return next_event
 
+    def __event_routine(self, event):
+        current_flow = event.associated_flow
+        type = event.event_type
 
+        if type == "ARRIVAL" or type == "HOP":
+            if self.architecture == "converged":
+                next_event_type, updated_flow = self.network.push_flow(current_flow, type)
+                if next_event_type != "BLOCKED":
+                    self.__schedule_event(self.sim_clock + updated_flow.hop_time, next_event_type, updated_flow)
+                else:
+                    self.blocked += 1
+            else:
+                next_event_type, updated_flow = self.network.push_flow(current_flow)
+                if next_event_type == "BLOCKED":
+                    self.__schedule_event(self.sim_clock + updated_flow.dur, "DEPART", updated_flow)
+                else:
+                    self.blocked += 1
+        else:
+            self.network.end_flow(current_flow)
+            self.sent += 1
 
+        if type == "ARRIVAL":
+            new_flow = self.flow_factory.generate_flow()
+            np.random.seed(self.seed)
+            iat = np.random.choice(self.expon_dist)
+            self.iat_times.append(iat)
+            self.__schedule_event(self.sim_clock + iat, "ARRIVAL", new_flow)
+
+    def run_simulation(self):
+        print(f"Starting {self.architecture} simulation")
+        self.start_time = time.time()
+        progress = 0
+        progress_bar = tqdm(total=self.min_flows, desc="Simulating", unit = "flow sent", leave=False)
+
+        while ((self.sent + self.blocked) < self.min_flows):
+            next_event = self.__timing_routine()
+            self.__event_routine(next_event)
+            progress = self.sent + self.blocked
+            progress_bar.update(progress - progress_bar.n)
+
+        self.end_time = time.time()
+        self.run_time = self.end_time - self.start_time
